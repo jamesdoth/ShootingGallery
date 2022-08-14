fdce412f6669afe317062ca51ea071a0fc6ece89
goforGUSTO <jamesdoth@gmail.com>
2022-08-14 13:06:54 -0700

    needs sound

---
+++ Game1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ShootingGallery
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D targetSprite;
+        Texture2D crosshairsSprite;
+        Texture2D backgroundSprite;
+        SpriteFont gameFont;
+
+        Vector2 targetPosition = new Vector2(300, 300);
+        const int targetRadius = 45;
+
+        MouseState mState;
+        bool mReleased = true;
+        int score = 0;
+
+        double timer = 10;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = false;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            targetSprite = Content.Load<Texture2D>("target");
+            crosshairsSprite = Content.Load<Texture2D>("crosshairs");
+            backgroundSprite = Content.Load<Texture2D>("sky");
+            gameFont = Content.Load<SpriteFont>("galleryFont");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (timer > 0)
+            {
+            timer -= gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if (timer < 0)
+            {
+                timer = 0;
+            }
+
+            mState = Mouse.GetState();
+
+            if (mState.LeftButton == ButtonState.Pressed && mReleased == true)
+            {
+                float mouseTargetDist = Vector2.Distance(targetPosition, mState.Position.ToVector2());
+                Random rand = new Random();
+                if (mouseTargetDist < 17 && timer > 0)
+                {
+                    score += 3;                    
+                    targetPosition.X = rand.Next(0, (_graphics.PreferredBackBufferWidth - 90));
+                    targetPosition.Y = rand.Next(0, (_graphics.PreferredBackBufferHeight - 90));
+                } else if (mouseTargetDist >= 17 && mouseTargetDist < 30 && timer > 0)
+                {
+                    score += 2;
+                    targetPosition.X = rand.Next(0, (_graphics.PreferredBackBufferWidth - 90));
+                    targetPosition.Y = rand.Next(0, (_graphics.PreferredBackBufferHeight - 90));
+                } else if (mouseTargetDist >= 30 && mouseTargetDist <= targetRadius && timer > 0)
+                {
+                    score++;
+                    targetPosition.X = rand.Next(0, (_graphics.PreferredBackBufferWidth - 90));
+                    targetPosition.Y = rand.Next(0, (_graphics.PreferredBackBufferHeight - 90));
+                }
+                mReleased = false;
+            }
+            if (mState.LeftButton == ButtonState.Released)
+            {
+                mReleased = true;
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White);
+            _spriteBatch.DrawString(gameFont, "Score: " + score.ToString(), new Vector2(3, 3), Color.White);
+            _spriteBatch.DrawString(gameFont, "Time: " + Math.Ceiling(timer).ToString(), new Vector2(3, 40), Color.White);
+            if (timer > 0)
+            {
+                _spriteBatch.Draw(targetSprite, new Vector2(targetPosition.X - targetRadius, targetPosition.Y - targetRadius), Color.White);
+            }
+
+            _spriteBatch.Draw(crosshairsSprite, new Vector2(mState.X - 25, mState.Y - 25), Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
